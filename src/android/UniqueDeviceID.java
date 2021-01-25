374ac7610fef849c46af5748eea225b29f4bca3e
Miguel Antunes <miguel.aka.kelter@gmail.com>
2021-01-25 16:55:34 +0000

    Update UniqueDeviceID.java

--- src/android/UniqueDeviceID.java
+++ src/android/UniqueDeviceID.java
@@ -40,7 +40,7 @@ public class UniqueDeviceID extends CordovaPlugin {
             String uuid = sharedPrefs.getString(PREF_UNIQUE_ID, null);
 
             if (uuid == null) {
-                uuid = UUID.randomUUID().toString();
+                uuid = generateDeviceIdentifier(context);
                 SharedPreferences.Editor editor = sharedPrefs.edit();
                 editor.putString(PREF_UNIQUE_ID, uuid);
                 editor.commit();
@@ -51,4 +51,86 @@ public class UniqueDeviceID extends CordovaPlugin {
             this.callbackContext.error("Exception occurred: ".concat(e.getMessage()));
         }
     }
+
+    /*
+     * Generate a new UDID
+     */
+    private String generateUDID(Context context) {
+        
+        //Try to get the ANDROID_ID
+        String UDID = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID); 
+        if (UDID == null || UDID.equals("9774d56d682e549c") || UDID.length() < 15 ) {
+            //if ANDROID_ID is null, or it's equals to the GalaxyTab generic ANDROID_ID or bad, generates a new one
+            UDID = UUID.randomUUID().toString();
+        }
+
+
+    }
+
+    @SuppressWarnings("deprecation")
+    @SuppressLint("HardwareIds")
+    public static String generateDeviceIdentifier(Context context) {
+
+            String pseudoId = "35" +
+                    Build.BOARD.length() % 10 +
+                    Build.BRAND.length() % 10 +
+                    Build.CPU_ABI.length() % 10 +
+                    Build.DEVICE.length() % 10 +
+                    Build.DISPLAY.length() % 10 +
+                    Build.HOST.length() % 10 +
+                    Build.ID.length() % 10 +
+                    Build.MANUFACTURER.length() % 10 +
+                    Build.MODEL.length() % 10 +
+                    Build.PRODUCT.length() % 10 +
+                    Build.TAGS.length() % 10 +
+                    Build.TYPE.length() % 10 +
+                    Build.USER.length() % 10;
+
+            //Try to get the ANDROID_ID
+            String androidId = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID); 
+            if (androidId == null || androidId.equals("9774d56d682e549c") || androidId.length() < 15 ) {
+                //if ANDROID_ID is null, or it's equals to the GalaxyTab generic ANDROID_ID or bad, generates a new one
+                androidId = UUID.randomUUID().toString();
+            }
+
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            String btId = "";
+
+            if (bluetoothAdapter != null) {
+                btId = bluetoothAdapter.getAddress();
+            }
+
+            String longId = pseudoId + androidId + btId;
+
+            try {
+                MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+                messageDigest.update(longId.getBytes(), 0, longId.length());
+
+                // get md5 bytes
+                byte md5Bytes[] = messageDigest.digest();
+
+                // creating a hex string
+                String identifier = "";
+
+                for (byte md5Byte : md5Bytes) {
+                    int b = (0xFF & md5Byte);
+
+                    // if it is a single digit, make sure it have 0 in front (proper padding)
+                    if (b <= 0xF) {
+                        identifier += "0";
+                    }
+
+                    // add number to string
+                    identifier += Integer.toHexString(b);
+                }
+
+                // hex string to uppercase
+                identifier = identifier.toUpperCase();
+                return identifier;
+            } catch (Exception e) {
+                Log.e("TAG", e.toString());
+            }
+            return "";
+    }
+
 }
